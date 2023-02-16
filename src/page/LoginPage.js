177402a6b56677c64dfd49a8795ec1380eceda55
Darryll Chong <darryllchong1337@gmail.com>
2023-02-17 00:35:32 +0800

    Update LoginPage and RegisterPage

--- src/page/LoginPage.js
+++ src/page/LoginPage.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-//import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import LoginImage from '../assets/login_image.png';
 
 import './loginPage.css';
 
 const LoginPage = () => {
 
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  })
+  const [errors, setErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values));
+    setIsSubmit(true);
+  }
+
+  useEffect(() => {
+    console.log(errors);
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log(values);
+    }
+  }, [errors]);
+
+  const validate = (values) => {
+    const errors = {};
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required!";
+    }
+
+    return errors;
+  };
+
   const navigate = useNavigate();
 
   return (
     <div className="login-page">
+      {Object.keys(errors).length === 0 && isSubmit ? (<div>placeholder for sign in success</div>) : null}
+      <pre>{JSON.stringify(values, undefined, 2)} FOR DEBUG</pre>
       <div className="login-form-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="login-form">
             <h1>Login</h1>
             <div className="input-field">
@@ -20,15 +58,21 @@ const LoginPage = () => {
               <input
                 type="text"
                 name="username"
-                placeholder="Username" />
+                placeholder="Username" 
+                value={setValues.username}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.username}</p>
             <div className="input-field">
               <label>Password</label>
               <input
                 type="password"
                 name="password"
-                placeholder="Password" />
+                placeholder="Password" 
+                value={setValues.password}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.password}</p>
             <button className="login-button">LOGIN</button>
             <button className="signup-button" onClick={() => navigate('/register/')}>SIGN UP</button>
           </div>
@@ -51,10 +95,4 @@ export default LoginPage
     const handleSubmit = (e) => {
         e.preventDefault();
     }
-
-    const onChange = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value })
-    }
-
-    console.log(values);
     */
--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -1,52 +1,120 @@
 import React from 'react';
-//import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import LoginImage from '../assets/login_image.png';
 
 import './registerPage.css';
 
 const RegisterPage = () => {
 
+  const [values, setValues] = useState({
+    fullname: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+  const [errors, setErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values));
+    setIsSubmit(true);
+  }
+
+  useEffect(() => {
+    console.log(errors);
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      console.log(values);
+    }
+  }, [errors]);
+  /*to edit validate*/
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.fullname = "Name is required!";
+    }
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required!";
+    }
+    if (!values.confirmPassword) {
+      errors.confirmPassword = "Please confirm your password!";
+    } else if (values.confirmPassword != values.password) {
+      errors.confirmPassword = "Password does not match!";
+    }
+
+    return errors;
+  };
+
+  const navigate = useNavigate();
+
   return (
     <div className="register-page">
       <div className="register-form-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="register-form">
             <h1>Create your Account</h1>
             <div className="input-field">
               <label>Name</label>
               <input
                 type="text"
-                name="name"
-                placeholder="Name" />
+                name="fullname"
+                placeholder="Name"
+                value={setValues.fullname}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.fullname}</p>
             <div className="input-field">
               <label>Username</label>
               <input
                 type="text"
                 name="username"
-                placeholder="Username" />
+                placeholder="Username"
+                value={setValues.username}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.username}</p>
             <div className="input-field">
               <label>Email</label>
               <input
                 type="email"
                 name="email"
-                placeholder="Email" />
+                placeholder="Email"
+                value={setValues.email}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.email}</p>
             <div className="input-field">
               <label>Password</label>
               <input
                 type="password"
                 name="password"
-                placeholder="Password" />
+                placeholder="Password"
+                value={setValues.password}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.password}</p>
             <div className="input-field">
               <label>Confirm Password</label>
               <input
                 type="password"
                 name="confirmPassword"
-                placeholder="Confirm password" />
+                placeholder="Confirm password"
+                value={setValues.confirmPassword}
+                onChange={handleChange}/>
             </div>
+            <p>{errors.confirmPassword}</p>
             <button className="register-button">REGISTER</button>
           </div>
         </form>
